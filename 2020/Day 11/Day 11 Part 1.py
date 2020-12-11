7d0571ac0854508f2856d440736259cbbfb55a69
Void <voidvenommc@gmail.com>
2020-12-11 01:35:07 -0500

    Uploaded 2020/Day 11

---
+++ 2020/Day 11/Day 11 Part 1.py
@@ -0,0 +1,57 @@
+import copy
+
+with open('input.txt', 'r') as input:
+	rows = list(input.read().split())
+	seats = list(map(list, rows))
+
+def checkSurroundings(seats, x, y):
+
+	occupiedCount = 0
+	for dX in range(-1, 2):
+		for dY in range(-1, 2):
+			if x + dX not in range(len(seats)):
+				continue
+			elif y + dY not in range(len(seats[0])):
+				continue
+
+			if dX == 0 and dY == 0:
+				continue
+			else:
+				if seats[x + dX][y + dY] == '#':
+					occupiedCount += 1
+
+	return occupiedCount
+
+def applyRules(seats):
+
+	newSeats = copy.deepcopy(seats)
+
+	changeCount = 0
+
+	for x in range(len(seats)):
+		for y in range(len(seats[x])):
+
+			surroundings = checkSurroundings(seats, x, y)
+			if seats[x][y] == 'L' and surroundings == 0:
+				newSeats[x][y] = '#' # occupado
+
+			elif seats[x][y] == '#' and surroundings >= 4:
+				newSeats[x][y] = 'L' # deoccupado
+			else:
+				continue
+
+			changeCount += 1 # if reached, neither of the change rules were met
+	return (newSeats, changeCount)
+
+seats = (seats, 1)
+while True:
+	seats = applyRules(seats[0])
+	
+	if seats[1] == 0:
+		break
+
+for i in range(len(seats[0])):
+	print(seats[0][i])
+
+# count the number of occupado seats
+print( sum(seats[0][i].count('#') for i in range(len(seats[0]))))
---
+++ 2020/Day 11/Day 11 Part 2.py
@@ -0,0 +1,68 @@
+import copy
+
+with open('input.txt', 'r') as input:
+	rows = list(input.read().split())
+	seats = list(map(list, rows))
+
+def getIndex(source, x, y):
+	if x not in range(0, len(source)):
+		return None
+	elif y not in range(0, len(source[x])):
+		return None
+	else:
+		return source[x][y]
+
+def checkSurroundings(seats, x, y):
+
+	occupiedCount = 0
+	for dX in range(-1, 2):
+		for dY in range(-1, 2):
+
+			if dX == 0 and dY == 0:
+				continue
+
+			else:
+				delta = [dX, dY]
+				while getIndex(seats, x + delta[0], y + delta[1]) == '.':
+					delta[0] += dX
+					delta[1] += dY
+				
+				if getIndex(seats, x + delta[0], y + delta[1]) == '#':
+					occupiedCount += 1
+
+	return occupiedCount
+
+def applyRules(seats):
+
+	newSeats = copy.deepcopy(seats)
+	changeCount = 0
+
+	for x in range(len(seats)):
+		for y in range(len(seats[x])):
+
+			surroundings = checkSurroundings(seats, x, y)
+			if seats[x][y] == 'L' and surroundings == 0:
+				newSeats[x][y] = '#' # occupado
+
+			elif seats[x][y] == '#' and surroundings >= 5:
+				newSeats[x][y] = 'L' # deoccupado
+
+			else:
+				continue
+
+			changeCount += 1 # if reached, neither of the change rules were met
+	return (newSeats, changeCount)
+
+
+seats = (seats, 1)
+while True:
+	seats = applyRules(seats[0])
+	
+	if seats[1] == 0:
+		break
+
+# count the number of occupado seats
+seats = seats[0]
+seatsLen = len(seats)
+
+print( sum(seats[i].count('#') for i in range(seatsLen)))
