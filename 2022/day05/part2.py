1bf42b87d3f13ebfc19f4c376e5679463e1253aa
Isaac Kilbourne <kilbouri@uwindsor.ca>
2022-12-05 01:06:38 -0500

    Actually parse input

--- 2022/day05/part2.py
+++ 2022/day05/part2.py
@@ -4,23 +4,42 @@ from pprint import pprint as print
 import re
 
 
+def parseStacks(stacks: list[str]) -> list[list[str]]:
+    """
+    Parses stacks like this:
+        [D]    
+    [N] [C]    
+    [Z] [M] [P]
+     1   2   3 
+
+    Into a list of stacks like this (first = bottom):
+    [
+        ['N', 'Z'], 
+        ['D', 'C', 'M'], 
+        ['P']]
+    """
+    # Start by prefixing each line with ' '. This will allow us to split the
+    # line into chunks of 4 chars, where each is either '    ', '  1 ', or ' [A]'.
+    stacks = [' ' + line for line in stacks]
+    stacks = [[line[i:i+4] for i in range(0, len(line), 4)] for line in stacks]
+
+    # Transpose the chunks so the list becomes a list of lists of chunks for each stack
+    stacks = map(list, zip(*stacks))
+
+    # Now drop the empty sections and the stack numbers from each chunk list
+    stacks = [[chunk.strip() for chunk in stack[:-1] if chunk.strip()] for stack in stacks]
+
+    # We now only have letters. Extract letters and we're done.
+    return [[chunk[1] for chunk in stack] for stack in stacks]
+
+
 def main():
     with open(f"{dirname(__file__)}/input.txt", "r") as file:
-        lines = file.read().splitlines()
+        stackSection, instructionsSection = map(str.splitlines, file.read().split('\n\n'))
 
-    stacks = [
-        list('GWLJBRTD'),
-        list('CWS'),
-        list('MTZR'),
-        list('VPSHCTD'),
-        list('ZDLTPG'),
-        list('DCQJZRBF'),
-        list('RTFMJDBS'),
-        list('MVTBRHL'),
-        list('VSDPQ'),
-    ]
+    stacks = parseStacks(stackSection)
 
-    for line in lines:
+    for line in instructionsSection:
         parse = re.match(r'move (\d+) from (\d+) to (\d+)', line)
         count, source, dest = map(int, parse.groups())
 
